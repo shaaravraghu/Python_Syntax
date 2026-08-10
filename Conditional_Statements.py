09743893cb3b2f96cbc5f711525a2a4547a7d698
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-10 12:11:39 +0530

    Update Conditional_Statements.py

--- Conditional_Statements.py
+++ Conditional_Statements.py
@@ -65,3 +65,93 @@ if 10 <= x <= 20:
 if 10 <= x and x <= 20: # long form
 a < b < c < d
 a < b and b < c and c < d # long form
+# a (value/ object) in b (object): True/ False
+
+
+# NESTED TERNARY
+result = (
+    "A" if x > 90
+    else "B" if x > 80
+    else "C"
+)
+# equivalent form
+if x > 90:
+    result = "A"
+elif x > 80:
+    result = "B"
+else:
+    result = "C"
+
+
+
+
+
+
+
+# WALRUS OPERATOR
+if (n := len("Python")) > 5:
+    print(n)
+#  without walrus
+n = len("Python")
+if n > 5:
+    print(n)
+# common use case
+while (line := input("Enter: ")) != "quit":
+    print(line)
+
+
+
+
+
+
+
+
+
+
+# MATCH/ CASE: SWITCH EQUIVALENT
+# More powerful than switch: Because you can match structure + values + extract data at the same time.
+# runs until 1st true case is hit (exempts other true/ false cases after 1st case hits true)
+point = (0, 5)
+match point:
+    case (0, 0):
+        print("Origin")
+    case (0, y):
+        print("On Y-axis:", y)
+    case (x, 0):
+        print("On X-axis:", x)
+    case (x, y):
+        print("Somewhere else:", x, y)
+# point
+#   ↓
+# (0, 5)
+# case (0, 0)   ❌
+# case (0, y)   ✅
+#               ↓
+#              y = 5
+#  Simpler way to remember
+match value:
+    case pattern:
+        action
+# case isn't necessarily asking:
+# "Is this exactly equal to X?"
+# It can ask:
+# "Does this value have this particular structure?"
+case 5:             # exactly 5
+case (0, y):        # tuple: 0 followed by anything
+case (x, 0):        # tuple: anything followed by 0
+case [x, y, z]:     # list with exactly 3 items
+case _:             # anything
+# multiple patterns
+match x:
+    case 1 | 2 | 3:
+        print("x is 1, 2, or 3")
+    case _:
+        print("something else")
+
+
+
+
+
+
+
+
