63ebea85f272342f0f66e4d3b51f2c3a2da2a310
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-11 14:59:07 +0530

    Enhance Lists-Tuples.py with indexing and slicing examples
    Added comments and examples for list indexing, modification, and slicing.

--- Lists-Tuples.py
+++ Lists-Tuples.py
@@ -7,3 +7,26 @@ nested = [[1, 2], [3, 4], [5, 6]]
 # Empty
 empty = []
 empty = list()
+
+
+
+
+
+
+
+
+
+
+
+# Index, Access, Modification, Slicing
+# Index:    0    1    2    3    4
+#           ↓    ↓    ↓    ↓    ↓
+# List:    10   20   30   40   50
+#          -5   -4   -3   -2   -1
+list1[i] # access
+# modification
+numbers = [10, 20, 30]
+numbers[1] = 100
+numbers[0:2] = [50, 60]
+# slicing
+list[start:stop:step]
