177f1a6827ab9fccae116238432a55c4fae66fc5
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-11 11:04:57 +0530

    Implement string examples and escape sequences
    Added various string creation examples and escape sequences.

---
+++ Strings.py
@@ -0,0 +1,28 @@
+# Creating strings, with quotes, escape sequences, raw string, type, index-sequence
+s = "Hello"
+s = 'Hello'
+s = """Hello
+World"""
+s = '''Hello
+World'''
+s = "I'm learning Python"
+s = 'He said "Hello"'
+s = 'I\'m learning Python'
+s = "He said \"Hello\""
+s = "He said \"Hello\""
+# | Escape | Meaning         |
+# | ------ | --------------- |
+# | `\n`   | Newline         |
+# | `\t`   | Tab             |
+# | `\\`   | Backslash       |
+# | `\'`   | Single quote    |
+# | `\"`   | Double quote    |
+# | `\r`   | Carriage return |
+# | `\b`   | Backspace       |
+# | `\0`   | Null character  |
+# raw string
+s = r"C:\Users\Shaarav\Documents"
+print(type(s)) # <class 'str'>
+# P  y  t  h  o  n
+# 0  1  2  3  4  5
+# -6 -5 -4 -3 -2 -1
