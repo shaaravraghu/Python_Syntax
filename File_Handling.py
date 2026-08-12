5ff4f5a7a86be526cdb7d6efbdbff12b56953956
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-12 18:34:18 +0530

    Reading a File

--- File_Handling.py
+++ File_Handling.py
@@ -19,3 +19,28 @@ open("data.txt", "r")
 # rb  read binary (preferable for non-text files)
 # wb  write binary (preferable for non-text files)
 
+# Reading a File
+# Entire File
+with open("data.txt", "r") as f:
+    content = f.read()
+# Fixed Number of Characters
+with open("data.txt", "r") as f:
+    data = f.read(10)
+# readline()
+with open("data.txt", "r") as f:
+    line1 = f.readline()
+    line2 = f.readline() # call multiple times if needed
+    # string ends with \n
+# readlines()
+with open("data.txt", "r") as f:
+    lines = f.readlines()
+    # creates list with strings (with \n)
+# Remove /n escape sequence
+with open("data.txt") as f:
+    for line in f:
+        line = line.strip()
+        print(line)
+# Itertae over a File
+with open("data.txt", "r") as f:
+    for line in f:
+        print(line)
